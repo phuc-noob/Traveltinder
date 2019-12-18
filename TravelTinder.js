15e2f0a8a065ea2bb4f423d6f075a9f0a504c2d2
huonganh0303 <huonganh0303@yahoo.com>
2019-12-19 00:16:12 +0700

    Find your destination

--- TravelTinder.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('ul.tabs li').click(function() {
-      var tab_id = $(this).attr('data-tab');
-      $('ul.tabs li').removeClass('current');
-      $('.form-content').removeClass('current');
-      $(this).addClass('current');
-      $("#" + tab_id).addClass('current');
-    })
-  });
